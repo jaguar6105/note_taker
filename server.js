f72a55eb4990d1a42caa1c87f9bd5daf57abc3a8
jaguar6105 <gilljoseph603@gmail.com>
2020-11-03 15:29:36 -0500

    cannot enter nots with the same id

--- server.js
+++ server.js
@@ -17,41 +17,46 @@ const notes = [];
 
 
 // This is the home page
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+});
 
-  //
-app.get("/notes", function(req, res) {
+//
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
+});
 
-  app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function (req, res) {
     return res.json(notes);
-  });
+});
 
-  app.post("/api/notes", function(req, res){
+app.post("/api/notes", function (req, res) {
     let newNote = req.body;
 
     newNote.routeName = newNote.title.replace(/\s+/g, "").toLowerCase();
 
     console.log(newNote);
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].title == req.body.title) {
+            return;
+        }
+    }
 
     notes.push(newNote);
 
     res.json(newNote);
-  });
-  
+});
 
-app.delete("/api/notes/:id", function(req, res){
-    for(let i = 0; i < notes.length; i++) {
-        if(notes[i].title === req.params.id) {
+
+app.delete("/api/notes/:id", function (req, res) {
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].title === req.params.id) {
             notes.splice(i, 1);
         }
     }
 });
-  
-  app.listen(PORT, function() {
+
+app.listen(PORT, function () {
     console.log("App listening on PORT http://localhost:" + PORT);
-  });
-  
+});
+
