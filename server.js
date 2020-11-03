2c9568f19e748b2ad25d14652cdd834ab05142af
jaguar6105 <gilljoseph603@gmail.com>
2020-11-03 16:25:26 -0500

    add comment

--- server.js
+++ server.js
@@ -21,21 +21,22 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-//
+//serve the file notes.html
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+//returns the array notes as a json file
 app.get("/api/notes", function (req, res) {
     return res.json(notes);
 });
 
+//posts the notes input to api
 app.post("/api/notes", function (req, res) {
     let newNote = req.body;
 
     newNote.routeName = newNote.title.replace(/\s+/g, "").toLowerCase();
 
-    console.log(newNote);
     for (let i = 0; i < notes.length; i++) {
         if (notes[i].title == req.body.title) {
             return;
@@ -47,7 +48,7 @@ app.post("/api/notes", function (req, res) {
     res.json(newNote);
 });
 
-
+//deletes notes array
 app.delete("/api/notes/:id", function (req, res) {
     for (let i = 0; i < notes.length; i++) {
         if (notes[i].title === req.params.id) {
